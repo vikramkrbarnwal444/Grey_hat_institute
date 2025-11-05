c2d702161b05c48d3dbc1e771d4192927bcf77d0
vikramkrbarnwal444 <barnwalvikramkumar337@gmail.com>
2025-11-05 22:51:11 +0530

    Delete apps directory

--- apps/courses/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Course
-
-@admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'duration', 'price')
-    search_fields = ('title', 'description')
